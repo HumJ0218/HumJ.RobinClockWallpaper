e7ca061c614c5c717db0aeccdb758d4d484596a2
HumJ <tlos142857@live.cn>
2024-05-08 03:10:01 +0800

    棒棒哒~

---
+++ HumJ.RobinClockWallpaper/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.Run();
